a88610a23148e367da8adc076b44b2d4c91c488d
yehor96 <yehor.shyriaiev@gmail.com>
2021-10-26 13:11:00 +0300

    refactor imports

--- src/test/java/com/luxoft/datastructures/queue/ArrayQueueTest.java
+++ src/test/java/com/luxoft/datastructures/queue/ArrayQueueTest.java
@@ -2,7 +2,10 @@ package com.luxoft.datastructures.queue;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class ArrayQueueTest {
 
